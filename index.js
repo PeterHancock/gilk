6e189c2db41571a7a22d02cc1d6b2eb0c5826959
Peter Hancock <peter.hancock@gmail.com>
2015-05-28 09:17:03 +0100

    Improved parallelism

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
-require('shelljs/global');
-
 var Promise = require('es6-promise').Promise,
-    fs = require('fs'),
+    denodeify = require('es6-denodeify')(Promise),
+    fs = require('fs-extra'),
     path = require('path'),
     _ = require('underscore'),
     docco = require('docco'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    copy = denodeify(fs.copy),
+    mkdirs = denodeify(fs.mkdirs);
 
 var root = __dirname;
 var templates = root + '/templates/';
@@ -27,7 +28,7 @@ var publicDest = 'build/public/';
 */
 module.exports = function gilk(config) {
 
-    var files = config.files; //TODO validate
+    var files = config.files || [];
     var title = config.title || 'Home';
     var js = config.js;
     var css = config.css;
@@ -35,6 +36,31 @@ module.exports = function gilk(config) {
 
     function destination(file) {
         return path.join(doccoOutput, path.basename(file, path.extname(file)) + '.html');
+    };
+
+    function postProc(resolve, reject) {
+        var promises = files.map(function (file) {
+            var ext = path.extname(file),
+            specName = path.basename(file, ext),
+            specFile = path.basename(file);
+            return new Promise(function (resolve, reject) {
+                fs.readFile(doccoOutput + specName + '.html', function (err, content) {
+                    var $ = cheerio.load(content);
+                    if (css) {
+                        $('head').append('<link rel="stylesheet" media="all" href="custom.css">');
+                    }
+                    $('body').prepend(navbar);
+                    if (js) {
+                        $('body').append('<script type="text/javascript" src="custom.js"></script>');
+                    }
+                    $('body').append('<script type="text/javascript" src="' + specFile + '"></script>');
+                    fs.writeFile(publicDest + specName + '.html', $.html(), function () {
+                        copy(file, publicDest + specFile).then(resolve, reject);
+                    });
+                });
+            });
+        });
+        Promise.all(promises).then(resolve);
     };
 
     var sources = files.map(function (source) {
@@ -49,90 +75,61 @@ module.exports = function gilk(config) {
         sources: sources
     });
 
-    function postProc(resolve, reject) {
-        files.forEach(function (file) {
-            var ext = path.extname(file),
-            specName = path.basename(file, ext),
-            specFile = path.basename(file),
-            $ = cheerio.load(fs.readFileSync(doccoOutput + specName + '.html'));
+    return Promise.all([mkdirs(doccoOutput), mkdirs(publicDest)])
+        .then(function () {
 
-            if (css) {
-                $('head').append('<link rel="stylesheet" media="all" href="custom.css">');
+            var promises = [];
+
+            var documentSpecs = new Promise(function (resolve, reject) {
+                docco.document({
+                    args: files,
+                    output: doccoOutput,
+                    template: templates + 'page.jst',
+                    css: '?' // Supresses warning
+                }, function () {
+                    postProc(resolve, reject);
+                });
+
+            });
+
+            if (indexFile) {
+                var createIndex = new Promise(function (resolve, reject) {
+                    var ext = path.extname(indexFile),
+                    name  = path.basename(indexFile, ext),
+                    toc = _.template(fs.readFileSync(templates + 'toc.jst').toString())({
+                        title: 'Table of Contents',
+                        sources: sources
+                    });
+                    docco.document({
+                        args: [indexFile],
+                        output: doccoOutput,
+                        template: templates + 'page.jst',
+                        css: '?' // Supresses warning
+                    }, function () {
+                        var $ = cheerio.load(fs.readFileSync(doccoOutput + name + '.html'));
+                        $('body').prepend(navbar);
+                        if (sources.length > 1) {
+                            $('.header').append(toc);
+                        }
+                        fs.writeFileSync(publicDest +  'index.html', $.html());
+                        resolve();
+                    });
+                });
+                promises.push(createIndex);
             }
 
-            $('body').prepend(navbar);
+            promises.push(documentSpecs);
+
+            // Add resources to public dir
+            promises.push(copy(publicSrc, publicDest));
 
             if (js) {
-                $('body').append('<script type="text/javascript" src="custom.js"></script>');
+                promises.push(copy(js, publicDest + 'custom.js'));
             }
 
-            $('body').append('<script type="text/javascript" src="' + specFile + '"></script>');
-
-            fs.writeFileSync(publicDest + specName + '.html', $.html());
-            //TODO do this async
-            cp(file, publicDest + specFile);
+            if (css) {
+                promises.push(copy(css, publicDest + 'custom.css'));
+            }
+            return Promise.all(promises);
         });
-        resolve();
-    };
-
-
-    mkdir('-p', doccoOutput);
-
-    mkdir('-p', publicDest);
-
-    var documentSpecs = new Promise( function (resolve, reject) {
-        docco.document({
-            args: files,
-            output: doccoOutput,
-            template: templates + 'page.jst',
-            css: '?' // Supresses warning
-        }, function () {
-            postProc(resolve, reject);
-        });
-
-    });
-
-    var copyResources = new Promise(function (resolve, reject) {
-        // Add resources to public dir
-        cp('-R', publicSrc + '*', publicDest);
-
-        if (js) {
-            cp(js, publicDest + 'custom.js');
-        }
-
-        if (css) {
-            cp(css, publicDest + 'custom.css');
-        }
-        resolve();
-    });
-
-    var promises = [documentSpecs, copyResources];
-
-    if (indexFile) {
-        var createIndex = new Promise(function (resolve, reject) {
-            var ext = path.extname(indexFile),
-            name  = path.basename(indexFile, ext),
-            toc = _.template(fs.readFileSync(templates + 'toc.jst').toString())({
-                title: 'Table of Contents',
-                sources: sources
-            });
-            docco.document({
-                args: [indexFile],
-                output: doccoOutput,
-                template: templates + 'page.jst',
-                css: '?' // Supresses warning
-            }, function () {
-                var $ = cheerio.load(fs.readFileSync(doccoOutput + name + '.html'));
-                $('body').prepend(navbar);
-                if (sources.length > 1) {
-                    $('.header').append(toc);
-                }
-                fs.writeFileSync(publicDest +  'index.html', $.html());
-                resolve();
-            });
-        });
-        promises.push(createIndex);
-    }
-
-    return Promise.all(promises);
 }
